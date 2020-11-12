4916f2c11819408b78c0393251d32cca16248a86
diana maina <dianamainachui@gmail.com>
2020-11-12 05:13:06 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,92 +1,21 @@
+// Dependencies
 const express = require("express");
-const mysql = require("mysql");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controllers.js");
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 8080;
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Chui@gt2020",
-  database: "burger_db"});
+const PORT = process.env.PORT || 8080;
 
-  // Initiate MySQL Connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Routes
-  app.get("/burgers", function(req, res) {
-    connection.query("SELECT * FROM burgers ORDER BY id", function(err, result) {
-      if (err) throw err;
-      
-      var html = "<h1>Burgers Ordered BY ID</h1>";
-  
-      html += "<ul>";
-  
-      for (var i = 0; i < result.length; i++) {
-        html += "<li><p> ID: " + result[i].id + "</p>";
-        html += "<p> Name: " + result[i].name + "</p>";
-        html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-        html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-      }
-  
-      html += "</ul>";
-  
-      res.send(html);
-    });
-  });
-  
-  app.get("/burgers-chart", function(req, res) {
-    connection.query("SELECT * FROM actors ORDER BY coolness_points DESC", function(err, result) {
-      if (err) throw err;
-  
-      var html = "<h1>Burgers by Coolness</h1>";
-  
-      html += "<ul>";
-  
-      for (var i = 0; i < result.length; i++) {
-        html += "<li><p> ID: " + result[i].id + "</p>";
-        html += "<p> Name: " + result[i].name + "</p>";
-        html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-        html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-      }
-  
-      html += "</ul>";
-  
-      res.send(html);
-    });
-  });
-  
-  app.get("/attitude-chart/:att", function(req, res) {
-    connection.query("SELECT * FROM actors WHERE attitude = ?", [req.params.att], function(err, result) {
-      if (err) throw err;
-  
-      var html = "<h1>Actors With an Attitude of " + req.params.att + "</h1>";
-  
-      html += "<ul>";
-  
-      for (var i = 0; i < result.length; i++) {
-        html += "<li><p> ID: " + result[i].id + "</p>";
-        html += "<p> Name: " + result[i].name + "</p>";
-        html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-        html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-      }
-  
-      html += "</ul>";
-  
-      res.send(html);
-    });
-  });
-  
-  // Start our server so that it can begin listening to client requests.
-  app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+// Starts the server and tells it to listen to client requests
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
+
+// MIDDLEWARE
+app.use(express.static("public")); // Serve Static Content
+app.use(express.urlencoded( { extended: true } ) ); // Parse JSON
+app.use(express.json());
+app.engine("handlebars", exphbs( { defaultLayout: "main" } ) );
+app.set("view engine", "handlebars");
+app.use(routes);
